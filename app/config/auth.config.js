47532f5299f3f1c55ec55b8e9e1c9e1fac22d8e5
TypM <tapdecotap2512@gmail.com>
2025-05-29 17:53:27 +0700

    ignore

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,6 +1,8 @@
 //กำหนดค่าที่เกี่ยวกับการยืนยันตัวตนด้วย JWT
+require("dotenv").config();  // เพิ่มบรรทัดนี้ไว้บนสุดของไฟล์
+
 module.exports = {
-    secret: "TypM-secret-key",
-    jwtExpiration: 3600,           // 1 hour
-    jwtRefreshExpiration: 86400,   // 24 hours
-  };
+  secret: process.env.JWT_SECRET,
+  jwtExpiration: process.env.JWT_EXPIRATION,
+  jwtRefreshExpiration: process.env.JWT_REFRESH_EXPIRATION,
+};
--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 //config ใข้เพื่อเขื่อมต่อฐข้อมูล
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "pin555",
-  DB: "test_student_subject",
-  dialect: "postgres",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT,
   pool: {
     max: 5,
     min: 0,

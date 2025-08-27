946d78f51277bf4ffb5399229080bde297849047
TypM <tapdecotap2512@gmail.com>
2025-08-27 17:47:40 +0700

    edit DATABASE_URL

--- app/models/index.js
+++ app/models/index.js
@@ -4,7 +4,7 @@ const dbConfig = require("../config/db.config.js");
 
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DB_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   dialectOptions: {

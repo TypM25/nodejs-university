d7c9fe7b9e49526f2757ab7bbc7762a2c3beb2da
TypM <tapdecotap2512@gmail.com>
2025-08-27 19:04:20 +0700

    edit url socket.io

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express"); //ตัวช่วยสร้างเว็บเซิร์ฟเวอร์
 const bodyParser = require("body-parser"); //แปลงข้อมูลที่ส่งเข้ามาให้เป็น JavaScript object
-const cors = require("cors"); // ช่วยให้เปิดเว็บไซต์จากโดเมนอื่น 
+const cors = require("cors"); // ช่วยให้เปิดเว็บไซต์จากโดเมนอื่น
 const path = require("path"); //จัดการกับ path ของไฟล์
 const app = express(); //สร้างเซิร์ฟเวอร์ Express
 
-const authConfig = require('./app/config/auth.config');
-
+const authConfig = require("./app/config/auth.config");
 
 //-------------------------------------------------SOCKET.IO-----------------------------------------------------
 const { Server } = require("socket.io");
-const { createServer } = require('node:http');
+const { createServer } = require("node:http");
 const server = createServer(app); //สร้าง HTTP server จาก Express
-const socketHandler = require('./socket'); // โหลดฟังก์ชัน socket จากไฟล์อื่น
-//สร้าง socket server 
+const socketHandler = require("./socket"); // โหลดฟังก์ชัน socket จากไฟล์อื่น
+//สร้าง socket server
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:3000",
-      "https://nodejs-university.vercel.app"
-    ], // กำหนดให้ Next.js เข้าถึงได้
+    origin: ["http://localhost:3000", process.env.DATABASE_URL], // กำหนดให้ Next.js เข้าถึงได้
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -29,12 +25,14 @@ socketHandler(io); // ส่ง io ไปให้จัดการต่อ
 //--------------------------------------------------------------------------------------------------------------
 var corsOptions = {
   //อนุญาตให้ทุกโดเมนเข้าถึง API นี้ได้
-  origin: "*"
+  origin: "*",
 };
 
-
 // ให้ Express เสิร์ฟไฟล์จากโฟลเดอร์นี้ที่ URL `/files`
-app.use("/files", express.static(path.join(__dirname, "/resources/static/assets/uploads")));
+app.use(
+  "/files",
+  express.static(path.join(__dirname, "/resources/static/assets/uploads"))
+);
 
 //------------------------------------------------ติดตั้ง Middleware ต่าง ๆ-----------------------------------------------------
 // ตัวแปรไว้ใช้ทั่วโปรเจกต์ว่าโปรเจกต์อยู่ที่ไหน
@@ -50,8 +48,9 @@ app.use(express.urlencoded({ extended: true }));
 const db = require("./app/models");
 const Role = db.role;
 
-//true ลบdatabaseทุกครั้งที่run app 
-db.sequelize.sync({ force: false })
+//true ลบdatabaseทุกครั้งที่run app
+db.sequelize
+  .sync({ force: false })
   .then(async () => {
     console.log("Database synchronized without dropping tables!");
 
@@ -65,7 +64,6 @@ db.sequelize.sync({ force: false })
     console.error("Error syncing database:", error);
   });
 
-
 // กำหนด route พื้นฐาน
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to TypM application." });
@@ -74,14 +72,13 @@ app.get("/", (req, res) => {
 //นำเข้าเส้นทาง (Routes) จากไฟล์แยกและส่ง app ไปให้
 require("./app/routes/admin.routes")(app);
 require("./app/routes/student.routes")(app);
-require('./app/routes/teacher.routes')(app);
-require('./app/routes/auth.routes')(app);
-require('./app/routes/user.routes')(app);
-require('./cron/semesterUpdate')(app);
-
+require("./app/routes/teacher.routes")(app);
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+require("./cron/semesterUpdate")(app);
 
 //เริ่มรันเซิร์ฟเวอร์
-const port =  process.env.PORT || process.env.NODE_LOCAL_PORT || 9000;
+const port = process.env.PORT || process.env.NODE_LOCAL_PORT || 9000;
 //เริ่มเปิดให้คนเข้ามาใช้งานได้
 server.listen(port, () => {
   console.log(`Server is running on port ${port}.`);

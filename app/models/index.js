cc12042f7b1969532d2829780bc179bd3c64793b
TypM <tapdecotap2512@gmail.com>
2026-04-19 16:39:32 +0700

    initialize database models index file

--- app/models/index.js
+++ app/models/index.js
@@ -4,12 +4,30 @@ const dbConfig = require("../config/db.config.js");
 
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  logging: false, 
-  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
-});
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: 'postgres',
+//   protocol: 'postgres',
+//   logging: false, 
+//   dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+// });
+
+require("dotenv").config();
+
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+  }
+);
+
+module.exports = sequelize;
+
 
 
 const db = {};
